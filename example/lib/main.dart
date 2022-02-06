582c472a04e1d53fbb79f5313926ae559bdacecc
moho4141 <mohommed4141@gmail.com>
2022-02-06 23:57:39 +0200

    add dispose methode

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -17,9 +17,11 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        body: FlutterWebQrcodeScanner(),
+        body: FlutterWebQrcodeScanner(
+          onGetResult: (d) {},
+        ),
       ),
     );
   }
